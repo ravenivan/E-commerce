a4e25d028a9adada3245fec061775dd8a084f860
Ivan Lam <ravenivan1@gmail.com>
2024-02-04 18:51:03 -0500

    finished html and css

---
+++ app.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.querySelector(".menu").style.visibility = "visible";
+    document.querySelector(".menu").style.opacity = "1";
+}
+
+function closeMenu() {
+    document.querySelector(".menu").style.visibility = "hidden";
+    document.querySelector(".menu").style.opacity = "0";
+}
